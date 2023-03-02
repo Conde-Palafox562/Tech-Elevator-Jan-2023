512439de8fb8e8193dfa578344a992ec986705d9
Raul Conde <r.conde2017@gmail.com>
2023-03-02 16:57:11 -0500

    Submitting Week 6 Day 3 Exercise

--- module-2/08_DAO_Testing/exercise/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
+++ module-2/08_DAO_Testing/exercise/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
@@ -24,52 +24,115 @@ public class JdbcTimesheetDaoTests extends BaseDaoTests {
 
     @Before
     public void setup() {
+
         dao = new JdbcTimesheetDao(dataSource);
     }
 
     @Test
     public void getTimesheet_returns_correct_timesheet_for_id() {
-        Assert.fail();
+
+        Timesheet timesheet = dao.getTimesheet(1);
+        assertTimesheetsMatch(TIMESHEET_1, timesheet);
+
+        timesheet = dao.getTimesheet(2);
+        assertTimesheetsMatch(TIMESHEET_2, timesheet);
+
     }
 
     @Test
     public void getTimesheet_returns_null_when_id_not_found() {
-        Assert.fail();
+
+        Timesheet timesheet = dao.getTimesheet(99);
+        Assert.assertNull(timesheet);
+
     }
 
     @Test
     public void getTimesheetsByEmployeeId_returns_list_of_all_timesheets_for_employee() {
-        Assert.fail();
+
+        //Act
+        List<Timesheet> results = dao.getTimesheetsByEmployeeId(1);
+
+        //Assert- make sure we have 2 timesheets for employee 1
+        //like we see int h test data sql or in the constants above
+        Assert.assertEquals(2, results.size());
     }
 
     @Test
     public void getTimesheetsByProjectId_returns_list_of_all_timesheets_for_project() {
-        Assert.fail();
+
+        List<Timesheet> results = dao.getTimesheetsByProjectId(1);
+
+        Assert.assertEquals(3, results.size());
+
     }
 
     @Test
     public void createTimesheet_returns_timesheet_with_id_and_expected_values() {
-        Assert.fail();
+
+        Timesheet newTimesheet = new Timesheet(0,2,2,
+                LocalDate.parse("2021-02-01"),2.0, false, "Timesheet 5");
+
+        Timesheet result = dao.createTimesheet(newTimesheet);
+        Assert.assertTrue(result.getTimesheetId() > 0);
+        newTimesheet.setTimesheetId(result.getTimesheetId());
+        assertTimesheetsMatch(newTimesheet, result);
+
     }
 
     @Test
     public void created_timesheet_has_expected_values_when_retrieved() {
-        Assert.fail();
+
+        Timesheet expectedTimesheet = new Timesheet(4, 2, 2,
+                LocalDate.parse("2023-03-02"), 2.0, false, "Timesheet 4");
+
+        Timesheet result = dao.createTimesheet(expectedTimesheet);
+        Timesheet retrievedTimesheet = dao.getTimesheet(result.getTimesheetId());
+
+        assertTimesheetsMatch(result, retrievedTimesheet);
+
     }
 
     @Test
     public void updated_timesheet_has_expected_values_when_retrieved() {
-        Assert.fail();
+
+        Timesheet timesheetToUpdate = dao.getTimesheet(4);
+
+        timesheetToUpdate.setEmployeeId(1);
+        timesheetToUpdate.setProjectId(2);
+        timesheetToUpdate.setDateWorked(LocalDate.parse("2023-03-02"));
+        timesheetToUpdate.setHoursWorked(8);
+        timesheetToUpdate.setBillable(true);
+        timesheetToUpdate.setDescription("Timesheet 5");
+
+        dao.updateTimesheet(timesheetToUpdate);
+        Timesheet retrievedTimeSheet = dao.getTimesheet(4);
+
+        assertTimesheetsMatch(timesheetToUpdate, retrievedTimeSheet);
+
     }
 
     @Test
     public void deleted_timesheet_cant_be_retrieved() {
-        Assert.fail();
+
+        dao.deleteTimesheet(2);
+
+        Timesheet retrievedTimesheet = dao.getTimesheet(2);
+        Assert.assertEquals(null, retrievedTimesheet);
+
+        List<Timesheet> results = dao.getTimesheetsByEmployeeId(2);
+        Assert.assertEquals(2, results.size());
+        assertTimesheetsMatch(TIMESHEET_3, results.get(0));
+
     }
 
     @Test
     public void getBillableHours_returns_correct_total() {
-        Assert.fail();
+
+        double billableHours = dao.getBillableHours(2,2);
+
+        Assert.assertEquals(0, billableHours, 2);
+
     }
 
     private void assertTimesheetsMatch(Timesheet expected, Timesheet actual) {
