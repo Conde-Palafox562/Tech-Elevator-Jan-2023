9db289b33f1c5e6ab0094210e17905366876dd17
Raul Conde <r.conde2017@gmail.com>
2023-03-06 11:57:48 -0500

    Lecture Update

--- module-2/11_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/services/HotelService.java
+++ module-2/11_Consuming_RESTful_APIs_Part_1/lecture/src/main/java/com/techelevator/hotels/services/HotelService.java
@@ -8,26 +8,43 @@ import org.springframework.web.client.RestTemplate;
 public class HotelService {
 
     private static final String API_BASE_URL = "http://localhost:3000/";
+
+    // This is similar to our JDBC template. It is out "waiter" that will take our order
+    // (in this case, an HTTP request) and deliver it to the kitchen (server) and then return
+    // our food (results)
     private final RestTemplate restTemplate = new RestTemplate();
 
     public Hotel[] listHotels() {
-        return null;
+
+        //define the path we want to call
+        String path = API_BASE_URL + "hotels";
+
+        return restTemplate.getForObject(path, Hotel[].class);
     }
 
     public Review[] listReviews() {
-        return null;
+
+        String path = API_BASE_URL + "reviews";
+        return restTemplate.getForObject(path, Review[].class);
+
     }
 
     public Hotel getHotelById(int id) {
-        return null;
+
+        String path = API_BASE_URL + "hotels/" + id;
+        return restTemplate.getForObject(path, Hotel.class);
     }
 
     public Review[] getReviewsByHotelId(int hotelId) {
-        return null;
+
+        String path = API_BASE_URL + "hotels/" + hotelId + "/reviews";
+        return restTemplate.getForObject(path, Review[].class);
     }
 
     public Hotel[] getHotelsByStarRating(int stars) {
-        return null;
+
+        String path = API_BASE_URL + "hotels?stars=" + stars;
+        return restTemplate.getForObject(path, Hotel[].class);
     }
 
     public City getWithCustomQuery(){
